513f13a0acbe86f938953a97a0f4c5e4dd03a9b0
faishal <faishalmahardika0547@gmail.com>
2024-01-09 23:47:42 +0700

    second

--- app/src/main/java/com/example/biker/KeyActivity.kt
+++ app/src/main/java/com/example/biker/KeyActivity.kt
@@ -2,10 +2,28 @@ package com.example.biker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.widget.Button
+import android.widget.Toast
 
 class KeyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_key)
+
+        val btn_toast1: Button = findViewById(R.id.btn_start)
+        val btn_toast2: Button = findViewById(R.id.btn_call)
+
+        btn_toast1.setOnClickListener {
+            showToast("Your Engine is ON")
+        }
+        btn_toast2.setOnClickListener {
+            showToast("Your Alarm is ON")
+        }
+
     }
+    private fun showToast(message: String){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/example/biker/MainActivity.kt
+++ app/src/main/java/com/example/biker/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.biker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNext = findViewById<Button>(R.id.btn_kunci)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, KeyActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
+
+
 }
