f7d2b100e569e509b88017c49cf6a4c9d202aa59
faishal <faishalmahardika0547@gmail.com>
2024-01-10 01:44:39 +0700

    second

--- app/src/main/java/com/example/biker/ServiceActivity.kt
+++ app/src/main/java/com/example/biker/ServiceActivity.kt
@@ -2,10 +2,45 @@ package com.example.biker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 
 class ServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val numberEditText = findViewById<EditText>(R.id.numberEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+
+
+
+
+        addButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+            val number = numberEditText.text.toString()
+            val email = emailEditText.text.toString()
+
+            val tableRow = LayoutInflater.from(this).inflate(R.layout.table_row, null) as TableRow
+            tableRow.findViewById<TextView>(R.id.nameTextView).setText(number)
+            tableRow.findViewById<TextView>(R.id.numberTextView).setText(name)
+            tableRow.findViewById<TextView>(R.id.emailTextView).setText(email)
+
+            val removeButton = tableRow.findViewById<TableRow>(R.id.removeButton)
+
+            removeButton.setOnClickListener {
+                tableLayout.removeView(tableRow)
+            }
+
+            tableLayout.addView(tableRow)
+
+        }
     }
 }
